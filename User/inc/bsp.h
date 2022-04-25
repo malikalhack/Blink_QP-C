b41bb258256ac162c3b2267fb7c6b67f9ef810a4
Anton Chernov <malikalhack@gmail.com>
2022-04-25 19:01:24 +0300

    Added doxygen

--- User/inc/bsp.h
+++ User/inc/bsp.h
@@ -7,8 +7,22 @@
 
 #define BIT(offset) (1 << (offset))
 
+/**
+ * @details Board initialisation function. It allows you to configure
+ * the control registers of counters, ports, timers, etc.
+ */
 void board_init(void);
+
+/**
+ * @details The function of turning on the LED on the board. It allows you
+ * to light the only controllable LED on the board, located on port C, pin 13.
+ */
 void board_led_on(void);
+
+/**
+ * @details The function of turning off the LED on the board. It allows you
+ * to turn off the only controllable LED on the board, located on port C, pin 13.
+ */
 void board_led_off(void);
 
 #endif /* !BSP_H_ */
--- User/src/bsp.c
+++ User/src/bsp.c
@@ -1,5 +1,6 @@
 #include "bsp.h"
 
+/** @fn board_init */
 void board_init(void) {
     RCC->APB1ENR |= RCC_APB1ENR_PWREN;
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
@@ -8,10 +9,12 @@ void board_init(void) {
     GPIOC->CRH |= GPIO_CRH_MODE13_1;
 }
 
+/** @fn board_led_off */
 void board_led_off(void) {
     GPIOC->BSRR = GPIO_ODR_ODR13;
 }
 
+/** @fn board_led_on */
 void board_led_on(void) {
     GPIOC->BRR = GPIO_ODR_ODR13;
 }
